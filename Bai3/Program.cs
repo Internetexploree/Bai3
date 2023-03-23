7b5b1122907ac5d6b772174be52286f650be9024
Internetexploree <lequi1322@gmail.com>
2023-03-24 00:10:13 +0700

    Add project files.

---
+++ Bai3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap duong kinh: ");
+            float duongKinh = float.Parse(Console.ReadLine());
+            const double PI = 3.14;
+            float banKinh = duongKinh/2;
+            double chuVi = duongKinh * PI;
+            double dienTich = Math.Pow(duongKinh,2)/4*PI;
+            Console.WriteLine("Ban kinh cua hinh tron la: {0}", banKinh);
+            Console.WriteLine("Dien tich cua hinh tron la: {0}", dienTich);
+            Console.WriteLine("Chu vi cua hinh tron la: {0}", chuVi);
+            Console.ReadKey();
+        }
+    }
+}
